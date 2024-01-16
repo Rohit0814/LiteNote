52467be118f67f3efa7ceac3867dcd2a01d20c87
rohit0814 <singhrohit.rs747@gmail.com>
2024-01-16 19:07:42 +0530

    complete major event of litenotes app

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TrashedNoteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,9 +20,36 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::resource('/notes', NoteController::class)->middleware('auth');
+
+// Route::get('/trashed',[TrashedNoteController::class, 'index'])->middleware('auth')->name('trashed.index');
+// Route::get('/trashed/{note}',[TrashedNoteController::class, 'show'])
+// ->withTrashed()
+// ->middleware('auth')->name('trashed.show');
+
+// Route::put('/trashed/{note}',[TrashedNoteController::class, 'update'])
+// ->withTrashed()
+// ->middleware('auth')->name('trashed.update');
+
+// Route::delete('/trashed/{note}',[TrashedNoteController::class, 'destroy'])
+// ->withTrashed()
+// ->middleware('auth')->name('trashed.destroy');
+
+
+Route::prefix('/trashed')->name('trashed.')->middleware('auth')->group(function(){
+    Route::get('/',[TrashedNoteController::class, 'index'])->name('index');
+    Route::get('/{note}',[TrashedNoteController::class, 'show'])->withTrashed()->name('show');
+
+    Route::put('/{note}',[TrashedNoteController::class, 'update'])->withTrashed()->name('update');
+
+    Route::delete('/{note}',[TrashedNoteController::class, 'destroy'])->withTrashed()->name('destroy');
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
